cd065b0cb8259dead3a0a66d238e9b746b0d6c4d
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2026-09-10 16:02:43 +0200

    Greetings, adventurer!

---
+++ AdventureGame/AdventureGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventureGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Robin!");
+        }
+    }
+}
