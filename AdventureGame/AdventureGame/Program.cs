3dc72c10c7dd1d2f3d8b20124932a658fa066d74
Robin Bertling <robinbertling10@gmail.com>
2026-09-11 09:51:19 +0200

    fixed loop pick char

--- AdventureGame/AdventureGame/Program.cs
+++ AdventureGame/AdventureGame/Program.cs
@@ -17,32 +17,35 @@ namespace AdventureGame
                 Console.WriteLine(charachter.Key + "  " +
                 charachter.Value.DisplayMenuName);
             }
-            bool userPickInpuit = false;
+            bool userPickInpuit = true;
             while (userPickInpuit)
             {
                 int.TryParse(Console.ReadLine(), out int selectedChar);
                 switch (selectedChar)
                 {
-                    case '1':
+                    case 1:
                         Console.WriteLine(championList.entityList[selectedChar].DisplayMenuName);
+                        userPickInpuit = false;
                         break;
 
-                    case '2':
+                    case 2:
                         Console.WriteLine("You pressed 2");
+                        userPickInpuit = false;
+
                         break;
 
-                    case '3':
+                    case 3:
                         Console.WriteLine("You pressed 3");
+                        userPickInpuit = false;
+
                         break;
 
                     default:
                         Console.WriteLine("Invalid choice");
-                        userPickInpuit = true;
                         break;
                 }
             }
 
-            Thread.Sleep(4000);
         }
     }
 }
